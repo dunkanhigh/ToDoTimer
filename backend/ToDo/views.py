b03c97777aa6ea62ee6bcbe3736c2797dab11994
dunkan_high <oleg.savinov.2002@bk.ru>
2022-12-24 14:58:36 +0300

    problem with auth

--- backend/ToDo/views.py
+++ backend/ToDo/views.py
@@ -1,9 +1,9 @@
 from rest_framework import viewsets
 
-from .models import ToDo
-from .serializers import ToDoSerializer
+# from .models import ToDo
+# from .serializers import ToDoSerializer
 
 
-class ToDoViewSet(viewsets.ModelViewSet):
-    queryset = ToDo.objects.all()
-    serializer_class = ToDoSerializer
+# class ToDoViewSet(viewsets.ModelViewSet):
+#     queryset = ToDo.objects.all()
+#     serializer_class = ToDoSerializer
